d0243698529cf5f6517cd0aa679acca95696e457
Dani Arthur Widodo <dani.widodo@krom.id>
2024-05-12 20:17:08 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import QRCode from 'qrcode';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ng-qr-code-generator';
+  input: any;
+
+  qrImage: string | undefined;
+  constructor() {
+   
+
+    
+  }
+
+  submitGenerateQr(){
+     // With async/await
+     const generateQR = async (text: any) => {
+      try {
+        this.qrImage = await QRCode.toDataURL(text)
+        console.log(await QRCode.toDataURL(text));
+      } catch (err) {
+        alert(err)
+        console.error(err);
+      }
+    };
+
+    generateQR(this.input);
+  }
 }
